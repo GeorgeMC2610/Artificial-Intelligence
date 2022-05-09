e18d42c94f659558570ea45d87f445774229e0c2
GeorgeMC2610 <giorgosseimenis@gmail.com>
2022-05-09 20:41:01 +0300

    Forgot to add title

--- Exercise 1/Automatic-Color-Filler/Form1.Designer.cs
+++ Exercise 1/Automatic-Color-Filler/Form1.Designer.cs
@@ -599,6 +599,7 @@
             this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Constraint Satisfaction Problem Solver";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize) (this.numericUpDownStartPop)).EndInit();
